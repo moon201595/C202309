f0474fad1d5343643a59bc98b3278be4c45c35fc
moon201595 <answnsgur030@naver.com>
2023-11-02 22:30:18 +0900

    issue

--- challenge/week5/functionMulti.c
+++ challenge/week5/functionMulti.c
@@ -8,6 +8,7 @@ int main()
 	printf("1부터 3까지의 곱은 %ld\n", multiF(3)); //1부터 3까지의 곱을 출력
 	printf("1부터 5까지의 곱은 %ld\n", multiF(5)); //1부터 5까지의 곱을 출력
 	
+	return 0;
 	
 }
 
--- practice/week5/functionHap.c
+++ practice/week5/functionHap.c
@@ -7,6 +7,7 @@ int main()
 	printf("1부터    10까지의 합은 %d\n", hapf(10)); //1부터 10까지의 합을 출력
 	printf("1부터  100까지의 합은 %d\n", hapf(100)); //1부터 100까지의 합을 출력
 	printf("1부터 1000까지의 합은 %d\n", hapf(1000)); //1부터 1000까지의 합을 출력
+	return 0;
 }
 
 int hapf(int value) //함수 hapf를 정의함
